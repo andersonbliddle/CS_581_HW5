3268ea6b4e53ea9856f65515df05f2b0fd22133c
Anderson Liddle <abliddle@crimson.ua.edu>
2024-11-26 17:04:20 -0600

    Updating job script

--- hw5_script.sh
+++ hw5_script.sh
@@ -2,11 +2,6 @@
 source /opt/asn/etc/asn-bash-profiles-special/modules.sh
 module load cuda/11.7.0
 
-echo "Serialized Code Runs:"
-
-./hw5_init 5000 5000 16 /scratch/ualclsd0173/validation_grid/ 1
-./hw5_init 10000 5000 16 /scratch/ualclsd0173/validation_grid/ 1
-
 echo "GPU 5000 Code Run 1:"
 ./hw5 5000 5000 1 /scratch/ualclsd0173/1/ 1
 ./hw5 5000 5000 2 /scratch/ualclsd0173/2/ 1
@@ -73,26 +68,31 @@ echo "GPU 10,000 Code Run 3:"
 ./hw5 10000 10000 32 /scratch/ualclsd0173/32/ 1
 ./hw5 10000 10000 64 /scratch/ualclsd0173/64/ 1
 
+echo "Serialized Code Runs:"
+
+./hw5_init 5000 5000 16 /scratch/ualclsd0173/validation_grid/ 1
+./hw5_init 10000 5000 16 /scratch/ualclsd0173/validation_grid/ 1
+
 echo "Running diffs between serial and GPU 5000 runs..."
 diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/1/output5000_5000_1.txt
-diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/1/output5000_5000_2.txt
-diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/1/output5000_5000_4.txt
-diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/1/output5000_5000_8.txt
-diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/1/output5000_5000_10.txt
-diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/1/output5000_5000_16.txt
-diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/1/output5000_5000_20.txt
-diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/1/output5000_5000_32.txt
-diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/1/output5000_5000_64.txt
+diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/2/output5000_5000_2.txt
+diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/4/output5000_5000_4.txt
+diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/8/output5000_5000_8.txt
+diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/10/output5000_5000_10.txt
+diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/16/output5000_5000_16.txt
+diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/20/output5000_5000_20.txt
+diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/32/output5000_5000_32.txt
+diff /scratch/ualclsd0173/validation_grid/output5000_5000_16_init.txt /scratch/ualclsd0173/64/output5000_5000_64.txt
 
 echo "Running diffs between serial and GPU 10,0000 runs..."
 diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/1/output10000_10000_1.txt
-diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/1/output10000_10000_2.txt
-diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/1/output10000_10000_4.txt
-diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/1/output10000_10000_8.txt
-diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/1/output10000_10000_10.txt
-diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/1/output10000_10000_16.txt
-diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/1/output10000_10000_20.txt
-diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/1/output10000_10000_32.txt
-diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/1/output10000_10000_64.txt
+diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/2/output10000_10000_2.txt
+diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/4/output10000_10000_4.txt
+diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/8/output10000_10000_8.txt
+diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/10/output10000_10000_10.txt
+diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/16/output10000_10000_16.txt
+diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/20/output10000_10000_20.txt
+diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/32/output10000_10000_32.txt
+diff /scratch/ualclsd0173/validation_grid/output10000_10000_16_init.txt /scratch/ualclsd0173/64/output10000_10000_64.txt
 
 echo "If you saw nothing, then there were no differences!"
